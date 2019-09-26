0856f6ba5332634b550b80c61d42aa263c7501c3
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-26 09:31:15 +0800

    v2

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const adding = require('./add');
 const path = require('path');
 const requestjs=require("./reques");
+app.listen('8080', console.log('listening to port 8080'));
+app.set('view engine', 'pug');
+
 
 app.use(function(req, res, next) {
     console.log(req.path)
@@ -11,29 +14,19 @@ app.use(function(req, res, next) {
     requestjs.hoi(req,res);
     next();
 });
-
-
-app.use(express.static(path.join( __dirname ,'./public/')));
-app.listen('8080', console.log('listening to port 8080'));
-app.set('view engine', 'pug');
-
-
+app.use(express.static(path.join( __dirname ,'/public')));
 app.get('/province/:provinceName', (req, res) => {
     fs.readFile('./province/' + req.params.provinceName + '.json', (e, data) => {
         if (e) {
             res.sendStatus(404);
         }
-
         var dats = JSON.parse(data);
         res.render('index', {dats});
     });
-
 });
 app.get('*', (req, res) => {
     res.sendStatus(404);
 });
-
-app.post('/rate', function(req, res) {
-   
+app.post('/rate', function(req, res) { 
      adding.addFile(req, res);
 })
--- reques.js
+++ reques.js
@@ -1,16 +1,11 @@
  const fs=require("fs");
  exports.hoi=function(req,res){
-    var cnt=0;
     fs.readFile('counter.txt', function(err, data) {
         if(err) throw err;
-        cnt = (data*1)+1;
-        fs.writeFile('counter.txt', cnt, (err) => {
-            if (err) throw err;
-            console.log(cnt);
-            res.end();
-        });  
-    });
-    
- 
-    
+        var cnt = (data*1)+1;
+    fs.writeFile('counter.txt', cnt, (err) => {
+        if (err) throw err;
+        console.log(cnt);
+    });  
+    });   
 }
